269ed872d88170e5fcb9b2bf3b9bcacdbf404a11
Dan Graney <Dan.Graney@eagleview.com>
2019-03-10 22:47:51 -0400

    Daily Verse now comes in KJV

--- scripts/dailyVerse.py
+++ scripts/dailyVerse.py
@@ -6,7 +6,16 @@ import json
 def getDailyVerse():
     url = 'http://labs.bible.org/api/?passage=votd&type=json'
     r = requests.get(url)
-    return r.json()[0]
+    dailyVerseJson = r.json()[0]
+    verseText = "%s %s:%s" % (dailyVerseJson['bookname'],dailyVerseJson['chapter'],dailyVerseJson['verse'])
 
+    verse = getVerse(verseText)
+    return verse
+
+def getVerse(verseRange):
+    url = 'https://bible-api.com/%s?translation=kjv' % verseRange
+    r = requests.get(url)
+    print(r.json())
+    return r.json()
 if __name__ == "__main__":
     getDailyVerse()
